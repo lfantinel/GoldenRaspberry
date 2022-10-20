3b6cc144d0810f5a7bbefafd79601998cf559244
Leandro Fantinel <fantinel@pop-os.localdomain>
2022-10-20 20:35:45 -0300

    first commit

---
+++ src/main/java/br/com/leandrofantinel/goldenraspberry/GoldenRaspberryApplication.java
@@ -0,0 +1,13 @@
+package br.com.leandrofantinel.goldenraspberry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoldenRaspberryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoldenRaspberryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/leandrofantinel/goldenraspberry/GoldenRaspberryApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.leandrofantinel.goldenraspberry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoldenRaspberryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
